9d4b9e00dc5a3f7b16d26be8985a5ae8f556f6bd
Pranay sharma <116100311+Brahamanbtp@users.noreply.github.com>
2026-03-19 05:23:36 +0000

    Add SelfIntrospection to consciousness modeling components

--- src/consciousness/__init__.py
+++ src/consciousness/__init__.py
@@ -3,11 +3,13 @@
 from .conscious_agent import ConsciousCryptographicAgent
 from .global_workspace import GlobalWorkspace
 from .integrated_info import IntegratedInformationCalculator
+from .introspection import SelfIntrospection
 from .metacognition import MetacognitiveMonitor
 
 __all__ = [
 	"ConsciousCryptographicAgent",
 	"GlobalWorkspace",
 	"IntegratedInformationCalculator",
+	"SelfIntrospection",
 	"MetacognitiveMonitor",
 ]
