51e9c8c257cce7db770fa933415dc4cf42ffe26c
Pranay sharma <116100311+Brahamanbtp@users.noreply.github.com>
2026-03-19 05:17:19 +0000

    Add MetacognitiveMonitor to consciousness modeling components

--- src/consciousness/__init__.py
+++ src/consciousness/__init__.py
@@ -3,5 +3,11 @@
 from .conscious_agent import ConsciousCryptographicAgent
 from .global_workspace import GlobalWorkspace
 from .integrated_info import IntegratedInformationCalculator
+from .metacognition import MetacognitiveMonitor
 
-__all__ = ["ConsciousCryptographicAgent", "GlobalWorkspace", "IntegratedInformationCalculator"]
+__all__ = [
+	"ConsciousCryptographicAgent",
+	"GlobalWorkspace",
+	"IntegratedInformationCalculator",
+	"MetacognitiveMonitor",
+]
