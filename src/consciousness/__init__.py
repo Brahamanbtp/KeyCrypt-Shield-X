60577a853418a2ef9b499d9981a48e0abaaa49c0
Pranay sharma <116100311+Brahamanbtp@users.noreply.github.com>
2026-03-19 05:15:00 +0000

    Add ConsciousCryptographicAgent to consciousness modeling components

--- src/consciousness/__init__.py
+++ src/consciousness/__init__.py
@@ -1,6 +1,7 @@
 """Consciousness modeling components for KeyCrypt Shield X."""
 
+from .conscious_agent import ConsciousCryptographicAgent
 from .global_workspace import GlobalWorkspace
 from .integrated_info import IntegratedInformationCalculator
 
-__all__ = ["GlobalWorkspace", "IntegratedInformationCalculator"]
+__all__ = ["ConsciousCryptographicAgent", "GlobalWorkspace", "IntegratedInformationCalculator"]
